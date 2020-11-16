cec0f3b058419b04e086f6ba3664e74f418ac7a4
İlker Rişvan <ilkerrisvan@outlook.com>
2020-11-16 19:58:59 +0300

    Add files via upload

---
+++ many_to_one_text.py
@@ -0,0 +1,12 @@
+import glob
+import sys
+
+loc_texts = sys.argv[1] ## location of texts
+loc_output_text = sys.argv[2] ## your data as input
+
+read_files = glob.glob(f"{loc_texts}/*.txt")
+
+with open(loc_output_text, "wb") as outfile:
+    for f in read_files:
+        with open(f, "rb") as infile:
+            outfile.write(infile.read())
